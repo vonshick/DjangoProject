7b71e02a81b60735a24e579071be1e0242c7ddb0
vonshick <kubawasik@vp.pl>
2021-01-31 16:18:50 +0100

    Change integer to time field in Tournament model

--- my_project/my_app/models/tournament_model.py
+++ my_project/my_app/models/tournament_model.py
@@ -6,7 +6,7 @@ class Tournament(models.Model):
     id = models.AutoField(db_index=True, primary_key=True)
     name = models.CharField(max_length=30)
     start_date = models.DateField()
-    start_hour = models.IntegerField()
+    start_hour = models.TimeField()
     participants_limit = models.IntegerField()
 
     def get_absolute_url(self):
